1307c3abb4e1730b9736e1f738defb1517116a82
Florian Rakos <florian.rakos@yahoo.com>
2024-02-27 22:41:16 +0100

    Inference changes

--- yoloNAS/inference.py
+++ yoloNAS/inference.py
@@ -8,6 +8,7 @@ from super_gradients import setup_device
 from os import listdir
 import torch
 import glob
+import time
 
 
 def main():
@@ -19,23 +20,43 @@ def main():
     # parser.add_argument("-o", "--output_folder", help="Path to save output images with bounding boxes")
     # args = parser.parse_args()
 
+    size = "l"
+    dataset = "all"
 
-    model_path = "../inference_model" + "/" + os.listdir("../inference_model")[0]
-    input_folder = "../data/test/images"
-    output_folder = "../inference_output/nas"
-    MODEL_ARCH = "yolo_nas_l"
-    classes = ['Deer', 'Roe Deer', ] # 'Chamois', 'Wild Boar', 'Rabbit', 'Horse', 'Sika Deer', 'Buffalo','Sheep'
+    model_path = f'/workspace/YAD/results/{dataset}/nas_{size}_1/ckpt_best.pth'
+
+    input_folder = "/workspace/YAD/data/test/images"
+ 
+    output_folder = f"../inference_output/nas/{size}"
+    #output_folder = f"../inference_output/nas/test"
+
+    MODEL_ARCH = f"yolo_nas_{size}"
+    classes = ['Deer', 'Fallow Deer', 'Horse', 'Rabbit', 'Roe Deer', 'Wild Boar']
 
 
     # Load the trained model
     model = get_model(MODEL_ARCH, num_classes=len(classes), checkpoint_path=model_path)
     model.eval()
-   
-    image_files = glob.glob(os.path.join(input_folder, '*.png'))
-    images_predictions = model.predict(image_files, iou=0.5, conf=0.7)
-    images_predictions.show(box_thickness=2, show_confidence=True)
-    images_predictions.save(output_folder=output_folder)
 
+    image_files = glob.glob(os.path.join(input_folder, '*.jpg'))
+
+    timer = time.time()
+    # images_predictions = model.predict(image_files, iou=0.5, conf=0.7)
+
+    # images_predictions.show(box_thickness=2, show_confidence=True)
+    # images_predictions.save("testname", output_folder=output_folder)
+
+    print("Start prediction")
+    for image in image_files:
+        pred = model.predict(image, iou=0.5, conf=0.7)
+        pred.show(box_thickness=2, show_confidence=True)
+        pred.save(output_folder=output_folder)
+        os.rename(os.path.join(output_folder, "pred_0.jpg"), os.path.join(output_folder, os.path.basename(image)))
+    print("Prediction done!")
+
+    print(f"Time taken for inference: {time.time() - timer} seconds")
+    num_images = len(image_files)
+    print(f"ms per image: {(time.time() - timer) * 1000 / num_images}")
 
 if __name__ == "__main__":
     main()
--- yoloV8/inference.py
+++ yoloV8/inference.py
@@ -2,21 +2,30 @@ import ultralytics
 from ultralytics import YOLO
 import subprocess
 
-model_used = "s"
+model_used = "m"
+dataset = "all"
+datadir = "data"
+
 
 ultralytics.checks()
-model = YOLO(f'../results/v8_{model_used}_1/weights/best.pt')
+model = YOLO(f'../results/{dataset}/v8_{model_used}_1/weights/best.pt')
 
-model.val(data='configs/dataDeerTest.yaml')
-model.predict(source='../dataDeer/test/images', save=True, save_txt=True, save_conf=True , conf=0.7, line_width=1) #imgsz=640
+model.val(data=f'configs/dataTest.yaml')
+model.predict(source=f'../{datadir}/test/images', save=True, save_txt=True, save_conf=True , conf=0.7, line_width=1) #imgsz=640
+
+
 
 src_inf= "/usr/src/ultralytics/runs/detect/predict"
-dest_inf = f"/workspace/inference_output/v8/{model_used}"
-cmd_inf = f"mv {src_inf} {dest_inf}"
-
 src_val = "/usr/src/ultralytics/runs/detect/val"
-dest_val = f"/workspace/inference_output/v8/{model_used}"
-cmd_val = f"mv {src_val} {dest_val}"
+dest = f"/workspace/inference_output/v8/{model_used}"
 
+cmd_inf = f"mv {src_inf} {dest}"
+cmd_val = f"mv {src_val} {dest}"
+cmd_chmod = f"chmod 777 -R {dest}"
+
+print("ExecuteCMD: ", cmd_inf)
 subprocess.run(cmd_inf, shell=True)
+print("ExecuteCMD: ", cmd_val)
 subprocess.run(cmd_val, shell=True)
+print("ExecuteCMD: ", cmd_chmod)
+subprocess.run(cmd_chmod, shell=True)
